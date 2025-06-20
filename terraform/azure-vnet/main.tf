ec8de450bbf16d60ae037881ddbc9b4242cb10fb
Gsingh4411 <Gurwinder.44@hotmail.com>
2025-06-20 12:36:15 -0700

    Add basic Terraform VNet example

---
+++ terraform/azure-vnet/main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "example-rg"
+  location = "Canada Central"
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
